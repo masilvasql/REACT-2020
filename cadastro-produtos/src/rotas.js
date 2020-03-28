62c7d10838c083f7219c5db01de9835ce4a011ad
Marcelo Abrão da Silva <masilvasql@gmail.com>
2020-03-28 20:04:57 -0300

    desenvolvido tela de visualização de produtos

--- cadastro-produtos/src/rotas.js
+++ cadastro-produtos/src/rotas.js
@@ -3,11 +3,13 @@ import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './views/Home'
 import CadastroProduto from './views/Produtos/cadastro'
+import ConsultaProdutos from './views/Produtos/consulta'
 
 export default () => (
     <HashRouter>
         <Switch>
             <Route exact path ="/cadastro-produtos" component = {CadastroProduto}/>
+            <Route exact path ="/consulta-produtos" component = {ConsultaProdutos}/>
             <Route exact path ="/" component = {Home} />
         </Switch>
     </HashRouter>
---
+++ cadastro-produtos/src/views/Produtos/consulta.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default class ConsultaProdutos extends React.Component {
+
+    state = {
+        produtos: []
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    Cadastro de Produto
+                    </div>
+                <div className="card-body">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>SKU</th>
+                                <th>Preço</th>
+                                <th>Fornecedor</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {
+                                this.state.produtos.map(produto => {
+                                    return (
+                                        <tr>
+                                            <th>{produto.nome}</th>
+                                            <th>{produto.sku}</th>
+                                            <th>{produto.preco}</th>
+                                            <th>{produto.fornecedor}</th>
+                                            <th></th>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        )
+    }
+}
