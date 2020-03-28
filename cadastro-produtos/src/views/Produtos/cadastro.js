d02f78fee026ddfd345ae9e3c5544e766e739ac4
Marcelo Abrão da Silva <masilvasql@gmail.com>
2020-03-28 12:27:15 -0300

    capturando dados do formulário

--- cadastro-produtos/src/views/Produtos/cadastro.js
+++ cadastro-produtos/src/views/Produtos/cadastro.js
@@ -1,6 +1,26 @@
 import React from 'react'
 
 export default class CadastroProduto extends React.Component {
+
+    state = {
+        nome: '',
+        sku: '',
+        descricao: '',
+        preco: 0,
+        fornecedor: ''
+    }
+
+    onChange = (event) => {
+        const valor = event.target.value
+        const nomeDoCampo = event.target.name
+        //setando valores dinamicamente
+        this.setState({ [nomeDoCampo]: valor })
+    }
+
+    onSubmit = (event) => {
+        console.log(this.state)
+    }
+
     render() {
         return (
             <>
@@ -14,13 +34,25 @@ export default class CadastroProduto extends React.Component {
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label>Nome: *</label>
-                                    <input type="text" className='form-control' />
+                                    <input
+                                        type="text"
+                                        className='form-control'
+                                        value={this.state.nome}
+                                        name="nome"
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label>SKU:</label>
-                                    <input type="text" className='form-control' />
+                                    <input
+                                        type="text"
+                                        className='form-control'
+                                        value={this.state.sku}
+                                        name="sku"
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -29,7 +61,12 @@ export default class CadastroProduto extends React.Component {
                             <div className="col-md-12">
                                 <div className="form-group">
                                     <label>Descrição:</label>
-                                    <textarea className="form-control" />
+                                    <textarea
+                                        className="form-control"
+                                        value={this.state.descricao}
+                                        name="descricao"
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -38,20 +75,35 @@ export default class CadastroProduto extends React.Component {
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label>Preço: *</label>
-                                    <input type="text" className='form-control' />
+                                    <input
+                                        type="text"
+                                        className='form-control'
+                                        value={this.state.preco}
+                                        name="preco"
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label>Fornecedor :*</label>
-                                    <input type="text" className='form-control' />
+                                    <input
+                                        type="text"
+                                        className='form-control'
+                                        value={this.state.fornecedor}
+                                        name="fornecedor"
+                                        onChange={this.onChange}
+                                    />
                                 </div>
                             </div>
                         </div>
 
                         <div className="row">
                             <div className="col-md-1">
-                                <button className="btn btn-success">Salvar</button>
+                                <button
+                                    className="btn btn-success"
+                                    onClick={this.onSubmit}
+                                >Salvar</button>
                             </div>
                             <div className="col-md-1">
                                 <button className="btn btn-primary">Limpar</button>
