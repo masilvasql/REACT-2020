f80ea6b382ae0deedaa679d9cc1def8f1c50a513
Marcelo Abrão da Silva <masilvasql@gmail.com>
2020-03-28 11:31:35 -0300

    importação bootswatch

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-     
+      <div className="alert alert-dismissible alert-warning">
+        <button type="button" className="close" data-dismiss="alert">&times;</button>
+        <h4 className="alert-heading">Warning!</h4>
+        <p className="mb-0"> Hello <a href="#" className="alert-link">BootSwatch</a>.</p>
+      </div>
     </div>
   );
 }
